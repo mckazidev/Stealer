33b94ec8fbb1d93dea375b5b5df836109eaf7a55
Kazi <flamycyber@gmail.com>
2024-05-21 11:24:33 +0300

    Create Category.java

---
+++ src/main/java/dev/kazi/stealer/node/Category.java
@@ -0,0 +1,12 @@
+package dev.kazi.stealer.node;
+
+public enum Category {
+
+    FILE("FILE");
+    
+    private final String name;
+    
+    private Category(final String name) {
+        this.name = name;
+    }
+}
